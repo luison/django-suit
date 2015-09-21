101afded6ddefabcbfa2be48b581c9a42e96a49a
nekmo <nekmo@alsur.es>
2015-09-21 20:28:36 +0200

    Acciones masivas.

--- suit/templatetags/suit_forms.py
+++ suit/templatetags/suit_forms.py
@@ -5,8 +5,13 @@ __author__ = 'nekmo'
 register = template.Library()
 
 
+def get_instance(element):
+    return element.__module__ + '.' + element.__class__.__name__
+
+register.filter('get_instance', get_instance)
+
 @register.filter(name='isinstance')
 def field_isinstance(element, compare):
     """Removes all values of arg from the given string"""
-    instance_name = element.__module__ + '.' + element.__class__.__name__
+    instance_name = get_instance(element)
     return instance_name == compare
