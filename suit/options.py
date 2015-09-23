cedd3ca0477e7d996493c01391bb5667850f1512
Jose <jose@alsur.es>
2015-09-23 18:44:22 +0200

    arreglo para instalacion en pip

--- suit/options.py
+++ suit/options.py
@@ -1,14 +1,13 @@
+__author__ = 'nekmo'
 from django.core.exceptions import ImproperlyConfigured
 try:
     from django.contrib.admin.options import FORMFIELD_FOR_DBFIELD_DEFAULTS
+    from django.db import models
+    from .widgets import SuitTextInputWidget, SuitIntegerWidget, AutosizedTextarea
 except ImproperlyConfigured:
     pass
-from django.db import models
-from .widgets import SuitTextInputWidget, SuitIntegerWidget, AutosizedTextarea
-
-__author__ = 'nekmo'
-
-FORMFIELD_FOR_DBFIELD_DEFAULTS[models.CharField] = {'widget': SuitTextInputWidget}
-FORMFIELD_FOR_DBFIELD_DEFAULTS[models.IntegerField] = {'widget': SuitIntegerWidget}
-FORMFIELD_FOR_DBFIELD_DEFAULTS[models.TextField] = {'widget': AutosizedTextarea}
-# FORMFIELD_FOR_DBFIELD_DEFAULTS[models.ForeignKey] = {'widget': SuitSelectWidget}
+else:
+    FORMFIELD_FOR_DBFIELD_DEFAULTS[models.CharField] = {'widget': SuitTextInputWidget}
+    FORMFIELD_FOR_DBFIELD_DEFAULTS[models.IntegerField] = {'widget': SuitIntegerWidget}
+    FORMFIELD_FOR_DBFIELD_DEFAULTS[models.TextField] = {'widget': AutosizedTextarea}
+    # FORMFIELD_FOR_DBFIELD_DEFAULTS[models.ForeignKey] = {'widget': SuitSelectWidget}
